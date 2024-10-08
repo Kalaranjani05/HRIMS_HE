5830479d5a3e52f43a76197e1e844f3d249d88d5
Kalaranjani <kalaranjani.he@enchhash.com>
2024-10-08 13:18:33 +0530

    url file changed

--- HRMS_login/src/Urls/Url.jsx
+++ HRMS_login/src/Urls/Url.jsx
@@ -1,9 +1,5 @@
 // login
 
 const login_post = "http://localhost:5000/api/Login";
-// const get_url = "http://localhost:4000/api/read";
-// const post_url = "http://localhost:4000/api/create";
-// const update_url = "http://localhost:4000/api/update";
-// const delete_url = "http://localhost:4000/api/delete";
 
 export { login_post };
